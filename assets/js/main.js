d242bdf6a129ba34c89db5a80169a29a2ca68e0a
vohraratik <vohraratik@gmail.com>
2023-05-25 20:25:37 +0530

    init2

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,6 +12,21 @@
       return document.querySelector(el);
     }
   };
+  /**
+   * Hero type effect
+   */
+  const typed = select(".typed");
+  if (typed) {
+    let typed_strings = typed.getAttribute("data-typed-items");
+    typed_strings = typed_strings.split(",");
+    new Typed(".typed", {
+      strings: typed_strings,
+      loop: true,
+      typeSpeed: 100,
+      backSpeed: 50,
+      backDelay: 2000,
+    });
+  }
 
   /**
    * Easy event listener function
